9244c463ce84d20c419e249c3e8a699146e6d004
Haixing <z5476259@ad.unsw.edu.au>
2024-02-22 17:34:10 +1100

    Finished stubbing for adminuserPasswordUpdate

--- src/auth.js
+++ src/auth.js
@@ -2,15 +2,31 @@
  * Given an admin user's authUserId and a set of properties, 
  * update the properties of this logged in admin user.
  * 
- * @param {int} authUserId - user ID
- * @param {string} email - email of user
- * @param {string} nameFirst - first name of user
- * @param {string} nameLast - last name of user
+ * @param {int} authUserId - ID of user
+ * @param {string} email - Email of user
+ * @param {string} nameFirst - First name of user
+ * @param {string} nameLast - Last name of user
  * 
- * @returns {}
+ * @returns {} - Returns empty object when properties is updated.
  */
 function adminUserDetailsUpdate( authUserId, email, nameFirst, nameLast ) {
     return {
 
+    }
+}
+
+/**
+ * Given details relating to a password change, 
+ * update the password of a logged in user.
+ * 
+ * @param {int} authUserId - ID of user
+ * @param {string} oldPassword - Old password of user
+ * @param {string} newPassword - New password of user
+ * 
+ * @returns {} - Returns empty object when password is updated.
+ */
+function adminUserPasswordUpdate( authUserId, oldPassword, newPassword ) {
+    return {
+        
     }
 }
