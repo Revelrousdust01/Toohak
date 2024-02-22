7468615a2bbcae65bb39dbff23748f66b7ef4205
Christian Politis <z5388072@ad.unsw.edu.au>
2024-02-22 19:15:08 +1100

    Changing mistake from string to number

--- src/quiz.js
+++ src/quiz.js
@@ -2,7 +2,7 @@
   * Update the description of the relevant quiz.
   * 
   * @param {string} authUserId - user id of admin
-  * @param {string} quizId - relevant quiz id
+  * @param {number} quizId - relevant quiz id
   * @param {string} description - new description for the relevant quiz
   * 
   * @returns {} - returns empty array when quiz description is updated
