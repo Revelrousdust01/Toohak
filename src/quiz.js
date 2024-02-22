c91c8326f698c443756a1c13db871083a6625d89
Jeffery Zhang <z5478440@ad.unsw.edu.au>
2024-02-22 18:12:02 +1100

    Edited style

--- src/quiz.js
+++ src/quiz.js
@@ -1,9 +1,9 @@
 /**
  * Given basic details about a new quiz, create one for the logged in user.
  * 
- * @param {int} authUserId 	- ID of user
+ * @param {int} authUserId - ID of user
  * @param {string} name - Name of user
- * @param {string} description 	- Basic details about new quiz.
+ * @param {string} description - Basic details about new quiz.
  * 
  * @returns {int} - Returns the quizID of the user.
  */
