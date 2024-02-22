396830ac80470524077e5566cda0443f239d4473
Christian Politis <z5388072@ad.unsw.edu.au>
2024-02-22 17:08:12 +1100

    Included description

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,12 @@
+/**
+  * Given a registered user's email and password returns their authUserId value.
+  * 
+  * @param {string} email - Email of user
+  * @param {string} password - Password of user
+  * 
+  * @returns {number} - Returns authUserId value when account is loged in
+*/
+
 function adminAuthLogin( email, password )
 {
     return{
