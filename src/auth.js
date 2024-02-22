2589a3a5e57936d2970cbec5c8f4895f4d02624f
Haixing <z5476259@ad.unsw.edu.au>
2024-02-22 18:29:32 +1100

    fixed function to get ready fopr merge

--- src/auth.js
+++ src/auth.js
@@ -1,26 +1,8 @@
 /**
- * Given an admin user's authUserId and a set of properties, 
- * update the properties of this logged in admin user.
- * 
- * @param {int} authUserId - ID of user
- * @param {string} email - Email of user
- * @param {string} nameFirst - First name of user
- * @param {string} nameLast - Last name of user
- * 
- * @returns {} - Returns empty object when properties is updated.
- */
-
-function adminUserDetailsUpdate( authUserId, email, nameFirst, nameLast ) {
-    return {
-
-    }
-}
-
-/**
  * Given details relating to a password change, 
  * update the password of a logged in user.
  * 
- * @param {int} authUserId - ID of user
+ * @param {number} authUserId - ID of user
  * @param {string} oldPassword - Old password of user
  * @param {string} newPassword - New password of user
  * 
