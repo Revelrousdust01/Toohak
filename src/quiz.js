b600517f2edad84b30c75edccefeb71e87357136
Jeffery Zhang <z5478440@ad.unsw.edu.au>
2024-02-22 18:53:58 +1100

    Update quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,11 +1,11 @@
 /**
  * Given basic details about a new quiz, create one for the logged in user.
  * 
- * @param {int} authUserId - ID of user
+ * @param {number} authUserId - ID of user
  * @param {string} name - Name of user
  * @param {string} description - Basic details about new quiz.
  * 
- * @returns {int} - Returns the quizID of the user.
+ * @returns {number} - Returns the quizID of the user.
  */
 function adminQuizCreate( authUserId, name, description ) {
 	return {
