20fb4f872606ee581b7278c6a293438dc79bbf1f
Christian Politis <z5388072@ad.unsw.edu.au>
2024-02-22 17:05:52 +1100

    Fix description

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,14 @@
+/**
+  * Register a user with an email, password, and names, then returns their authUserId value.
+  * 
+  * @param {string} email - Email of user
+  * @param {string} password - Password of user
+  * @param {string} nameFirst - First name of user 
+  * @param {string} nameLast - Last Name of user
+  * 
+  * @returns {number} - Returns authUserId value when account is registered
+*/
+
 function adminAuthRegister( email, password, nameFirst, nameLast )
 {
     return {
