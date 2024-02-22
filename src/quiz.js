27345761fc7791fedf4be5881502583e4acf5d38
Jeffery Zhang <z5478440@ad.unsw.edu.au>
2024-02-22 20:17:56 +1100

    Finshed adminQuizCreate

--- src/quiz.js
+++ src/quiz.js
@@ -1,10 +1,27 @@
 /**
+ * Given basic details about a new quiz, create one for the logged in user.
+ * 
+ * @param {int} authUserId - ID of user
+ * @param {string} name - Name of user
+ * @param {string} description - Basic details about new quiz.
+ * 
+ * @returns {int} - Returns the quizID of the user.
+ */
+
+function adminQuizCreate( authUserId, name, description ) {
+	return {
+		quizId: 2
+	}
+}
+      
+
+/**
  * Given a particular quiz, permanently remove the quiz.
  * 
- * @param {number} authUserId - User ID of admin
- * @param {number} quizId - relevant quiz ID
+ * @param {int} authUserId - User ID of admin
+ * @param {int} quizId - relevant quiz ID
  * 
- * @returns {} - returns empty object when removing quiz
+ * @returns {} - no return
  */
 
 function adminQuizRemove( authUserId, quizId ) {
