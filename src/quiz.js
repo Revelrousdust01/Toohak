a72c3b31eab56ce7a423eee2bf0fee69a65da4d6
Leon <z5480602@ad.unsw.edu.au>
2024-02-22 18:33:02 +1100

    Changed style and removed quiz list function

--- src/quiz.js
+++ src/quiz.js
@@ -3,34 +3,15 @@
   *  
   * Update the name of the relevant quiz.
   * 
-  * @param {string} authUserId  - user id of admin
-  * @param {string} quizId      - relevant quiz id
-  * @param {string} name        - new name for the relevant quiz
+  * @param {string} authUserId - user id of admin
+  * @param {string} quizId - relevant quiz id
+  * @param {string} name - new name for the relevant quiz
   * 
-  * @returns {}                 - no return
+  * @returns {} - no return
 */
 
 function adminQuizNameUpdate( authUserId, quizId, name ) {
     return {
         
     }
-}
-
- * Provide a list of all quizzes that are owned by the currently logged in user.
- * 
- * @param {int} authUserId              - ID of user
- * 
- * @returns {array}                     - Returns array of all quizzes that are owned
- *                                        by the currently logged in user.
- */
-
-function adminQuizList( authUserId ) {
-	return  { quizzes: 
-		[
-			{
-				quizId: 1,
-				name: 'My Quiz',
-			}
-		]
-	}
 }
