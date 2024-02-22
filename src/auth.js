47ca1b9e14e199b410072fb989762002d0a318da
Christian Politis <z5388072@ad.unsw.edu.au>
2024-02-22 17:14:21 +1100

    Documentation for the admin user details

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,12 @@
+/**
+  * Given an admin user's authUserId, return details about the user.
+  * "name" is the first and last name concatenated with a single space between them.
+  * 
+  * @param {number} authUserId - ID of user
+  * 
+  * @returns {Object} - Returns an object containing details about the user when the account is logged in.
+*/
+
 function adminUserDetails(authUserId) 
 {
   return {
