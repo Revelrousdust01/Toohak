50465f06fdbde0860993fdd278a356f5ac2f1b61
Haixing <z5476259@ad.unsw.edu.au>
2024-02-22 17:17:48 +1100

    Finished stubbing for adminUserDetailsUpdate

---
+++ src/auth.js
@@ -0,0 +1,16 @@
+/**
+ * Given an admin user's authUserId and a set of properties, 
+ * update the properties of this logged in admin user.
+ * 
+ * @param {int} authUserId - user ID
+ * @param {string} email - email of user
+ * @param {string} nameFirst - first name of user
+ * @param {string} nameLast - last name of user
+ * 
+ * @returns {}
+ */
+function adminUserDetailsUpdate( authUserId, email, nameFirst, nameLast ) {
+    return {
+
+    }
+}
