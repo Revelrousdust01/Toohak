1ac32d04da7490d55d4c6dee9f8c47f7915553eb
Leon Sun <z5480602@ad.unsw.edu.au>
2024-02-22 19:19:09 +1100

    Update quiz.js to change mistakes from string to number

--- src/quiz.js
+++ src/quiz.js
@@ -1,8 +1,8 @@
 /**
   * Update the name of the relevant quiz.
   * 
-  * @param {string} authUserId - user id of admin
-  * @param {string} quizId - relevant quiz id
+  * @param {number} authUserId - user id of admin
+  * @param {number} quizId - relevant quiz id
   * @param {string} name - new name for the relevant quiz
   * 
   * @returns {} - returns empty object when quiz name is updated
