6f932fc103afc2fb612e52f860a4917eb64a63e4
Leon <z5480602@ad.unsw.edu.au>
2024-02-22 18:27:46 +1100

    Changed style to match sample

--- src/quiz.js
+++ src/quiz.js
@@ -3,11 +3,11 @@
   *  
   * Update the description of the relevant quiz.
   * 
-  * @param {string} authUserId  - user id of admin
-  * @param {string} quizId      - relevant quiz id
+  * @param {string} authUserId - user id of admin
+  * @param {string} quizId - relevant quiz id
   * @param {string} description - new description for the relevant quiz
   * 
-  * @returns {}                 - no return
+  * @returns {} - no return
 */
 
 function adminQuizDescriptionUpdate( authUserId, quizId, description ) {
