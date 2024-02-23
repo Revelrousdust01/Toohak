88e28427ae38c07a643bef9647ea4b912cd28f4a
Leon <z5480602@ad.unsw.edu.au>
2024-02-23 12:40:13 +1100

    Created a clear function in other.js

---
+++ src/other.js
@@ -0,0 +1,13 @@
+/**
+  * Reset the state of the application back to the start.
+  * 
+  * @param {} - no parameters
+  * 
+  * @returns {} - returns an empty object when resetting the application to the start
+*/
+
+function clear() {
+    return {
+
+    }
+}
