e367421b536097e8941c48edc7082b4fecd37d9a
Jeffery Zhang <z5478440@ad.unsw.edu.au>
2024-03-03 16:59:53 +1100

    Bugfixed adminQuizCreate tests

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -1,4 +1,5 @@
 import { adminQuizCreate } from './quiz.js';
+import { adminAuthRegister } from './auth.js';
 import { clear } from './other.js';
 
 // Clear before each test
@@ -11,7 +12,9 @@ const ERROR = { error: expect.any(String) };
 describe('Test adminQuizCreate', () => {
     test('Valid inputs', () => {
         const admin = adminAuthRegister('jeffery.zhang385@gmail.com', 'str0ngpassword', 'Jeffery', 'Zhang');
-        expect(adminQuizCreate(admin.authUserId, 'New Quiz', 'This is a new quiz')).toStrictEqual(expect.any(Number));
+        const newQuiz = adminQuizCreate(admin.authUserId, 'New Quiz', 'This is a new quiz');
+        expect(newQuiz).toHaveProperty('quizId');
+        expect(newQuiz.quizId).toStrictEqual(expect.any(Number));
     });
     test('Invalid AuthUserId', () => {
         expect(adminQuizCreate(-999, 'New Quiz', 'This is a new quiz')).toStrictEqual(ERROR);
@@ -22,17 +25,17 @@ describe('Test adminQuizCreate', () => {
     });
     test('Name is too short or too long', () => {
         const admin = adminAuthRegister('jeffery.zhang385@gmail.com', 'str0ngpassword', 'Jeffery', 'Zhang');
-        expect(adminQuizCreate(admin.authUserId, 'NQ', 'This is a new quiz')).toStrictEqual(ERROR);
-        expect(adminQuizCreate(admin.authUserId, 'New Quiz'.repeat(10), 'This is a new quiz')).toStrictEqual(ERROR);
-
+        expect(adminQuizCreate(admin.authUserId, 'aa', 'This is a new quiz')).toStrictEqual(ERROR);
+        expect(adminQuizCreate(admin.authUserId, 'a'.repeat(31), 'This is a new quiz')).toStrictEqual(ERROR);
     });
     test('Name is already used', () => {
         const admin = adminAuthRegister('jeffery.zhang385@gmail.com', 'str0ngpassword', 'Jeffery', 'Zhang');
-        expect(adminQuizCreate(admin.authUserId, 'New Quiz', 'This is a new quiz')).toStrictEqual(expect.any(Number));
-        expect(adminQuizCreate(admin.authUserId, 'New Quiz', 'This is a new quiz')).toStrictEqual(ERROR);
+        console.log('This is where the test is');
+        const newQuiz = adminQuizCreate(admin.authUserId, 'New Quiz', 'This is a new quiz');
+        expect(adminQuizCreate(admin.authUserId, 'New Quiz', 'This is another new quiz')).toStrictEqual(ERROR);
     });
     test('Description is too long', () => {
         const admin = adminAuthRegister('jeffery.zhang385@gmail.com', 'str0ngpassword', 'Jeffery', 'Zhang');
-        expect(adminQuizCreate(admin.authUserId, 'New Quiz', 'This is a new quiz'.repeat(10))).toStrictEqual(ERROR);
+        expect(adminQuizCreate(admin.authUserId, 'New Quiz', 'a'.repeat(101))).toStrictEqual(ERROR);
     });
 });
