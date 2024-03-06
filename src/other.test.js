e6d44f57287f004168d885540717457dd27df540
Leon <z5480602@ad.unsw.edu.au>
2024-03-06 20:05:22 +1100

    Changed tests to reflect changes in data.md

--- src/other.test.js
+++ src/other.test.js
@@ -2,7 +2,6 @@ import { clear } from './other';
 import { adminAuthRegister, adminUserDetails } from './auth';
 import { adminQuizCreate, adminQuizList } from './quiz';
 
-// Clear before each test
 beforeEach(() => {
     clear();
 });
@@ -13,20 +12,20 @@ describe('Iteration 1: Test "clear" function ', () => {
     });
 
     test('Resets users list to empty', () => {
-        adminAuthRegister('leonsun@gmail.com', 'leonsunspassword', 'Leon', 'Sun');
+        adminAuthRegister('leonsun@gmail.com', 'Leonsunspassword1', 'Leon', 'Sun');
         clear();
         expect(clear()).toStrictEqual({});
     });
 
     test('Resets quizzes list to empty', () => {
-        const admin = adminAuthRegister('leonsun@gmail.com', 'leonsunspassword', 'Leon', 'Sun');
+        const admin = adminAuthRegister('leonsun@gmail.com', 'Leonsunspassword1', 'Leon', 'Sun');
         adminQuizCreate(admin.authUserId, 'Quiz 1', 'This is the first test quiz');
         clear();
         expect(clear()).toStrictEqual({});
     });
     
     test('Resets both quizzes list and users list to empty', () => {
-        const admin = adminAuthRegister('leonsun@gmail.com', 'leonsunspassword', 'Leon', 'Sun');
+        const admin = adminAuthRegister('leonsun@gmail.com', 'Leonsunspassword1', 'Leon', 'Sun');
         adminQuizCreate(admin.authUserId, 'Quiz 1', 'This is the first test quiz');
         clear();
         expect(clear()).toStrictEqual({});
@@ -34,18 +33,19 @@ describe('Iteration 1: Test "clear" function ', () => {
     });
 
     test('Ensure that user and quiz creations are independent post-clear', () => {
-        const admin = adminAuthRegister('leonsun@gmail.com', 'leonsunspassword', 'Leon', 'Sun');
+        const admin = adminAuthRegister('leonsun@gmail.com', 'Leonsunspassword1', 'Leon', 'Sun');
         adminQuizCreate(admin.authUserId, 'Quiz 1', 'This is the first test quiz');
         clear();
-        const admin2 = adminAuthRegister('leonsun1@gmail.com', 'leonsunspassword1', 'Leona', 'Sunny');
+        const admin2 = adminAuthRegister('leonsun1@gmail.com', 'Leonsunspassword1', 'Leona', 'Sunny');
         adminQuizCreate(admin2.authUserId, 'Quiz 2', 'This is the second test quiz');
         expect(adminUserDetails(admin2.authUserId)).toStrictEqual({
             userId: expect.any(Number),
             email: expect.any(String),
-            name: expect.any(String),
+            nameFirst: expect.any(String),
+            namelast: expect.any(String),
             numFailedPasswordsSinceLastLogin: expect.any(Number),
             numSuccessfulLogins: expect.any(Number),
-        })
+        });
         expect(adminQuizList(admin2.authUserId)).toStrictEqual({
             quizId: expect.any(String),
             name: expect.any(String),
