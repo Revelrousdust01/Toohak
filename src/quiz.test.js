881f8becf301ee0afb4d64c26d3d52e28cb67ef5
Haixing <z5476259@ad.unsw.edu.au>
2024-03-07 19:13:29 +1100

    finished test for adminQuizList

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -110,3 +110,67 @@ describe('Test adminQuizRemove', () => {
         expect(adminQuizRemove(admin.authUserId, quizId)).toStrictEqual(ERROR);
     });
 });
+
+describe('adminQuizList', () => {
+    let firstName = 'Samuel'
+    let lastName = 'Huang'
+    let email = 'shuang@student.unsw.edu.au'
+    let password = 'a1b2c3d4e5f6'
+    const UserId = adminAuthRegister(email, password, firstName, lastName)
+
+    test('One quiz in quizlist', () => {
+        const quizId1 = adminQuizCreate(UserId, 'How to train your dragon', 
+                        'Quiz about the movie trivia of How to Train your dragon')
+        expect(adminQuizList(UserId)).toStrictEqual({
+            quizzes: [
+                {
+                    quizId: quizId1,
+                    name: 'How to train your dragon'
+                }
+            ]
+        });
+    });
+
+    test('Multiple quiz in quizlist', () => {
+        const quizId1 = adminQuizCreate(UserId, 'How to train your dragon', 
+                        'Quiz about the movie trivia of How to Train your dragon')
+        const quizId2 = adminQuizCreate(UserId, 'Age of Adeline', 
+                        'Quiz about the movie trivia of Age of Adeline')
+        const quizId3 = adminQuizCreate(UserId, 'Kung Fu Panda', 
+                        'Quiz about the movie trivia of Kung Fu Panda')
+        expect(adminQuizList(UserId)).toStrictEqual({
+            quizzes: [
+                {
+                    quizId: quizId1,
+                    name: 'How to train your dragon'
+                },
+                {
+                    quizId: quizId2,
+                    name: 'Age of Adeline'
+                },
+                {
+                    quizId: quizId3,
+                    name: 'Kung Fu Panda'
+                }
+            ]
+        });
+    });
+
+    test('No quiz in quizlist', () => {
+        expect(adminQuizList(UserId)).toStrictEqual({
+            quizzes: [
+
+            ]
+        });
+    });
+
+    test.each([
+        { invalidId: '-1' },
+        { invalidId: 'a' },
+        { invalidId: '/' },
+    ])("AuthUserId is not a valid user: '$invalidId", ({ invalidId }) => {
+        const admin = adminAuthRegister(email, password, lastName, firstName);
+        const quizId = adminQuizCreate(admin.authUserId, quizName, quizDescription);
+	expect(adminQuizList(invalidId)).toStrictEqual(ERROR);
+    });
+});
