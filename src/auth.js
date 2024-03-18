0ff167b0dd6e6931134f74fd008d40fde0897903
Christian Politis <christianpolitis@gmail.com>
2024-03-18 18:28:57 +1100

    Fixed all linting issues

--- src/auth.js
+++ src/auth.js
@@ -1,216 +1,223 @@
-import {getData, setData} from './dataStore';
-import {validAuthUserId, validEmail, validName, validPassword} from './helper';
+import { getData, setData } from './dataStore';
+import { validAuthUserId, validEmail, validName, validPassword } from './helper';
 
 /**
   * Given a registered user's email and password returns their authUserId value.
-  * 
+  *
   * @param {string} email - Email of user
   * @param {string} password - Password of user
-  * 
+  *
   * @returns {number} - Returns authUserId value when account is loged in
 */
 
-export function adminAuthLogin( email, password )
-{
-    let data = getData();
-    const user = data.users.find(users => users.email == email)
+export function adminAuthLogin(email, password) {
+  const data = getData();
+  const user = data.users.find(users => users.email === email);
 
-    if(!user)
-        return { error: "No account found with the provided email address." }
-    else if(user.password !== password)
-        return { error: "Incorrect password." }
+  if (!user) { return { error: 'No account found with the provided email address.' }; } else if (user.password !== password) { return { error: 'Incorrect password.' }; }
 
-    return{
-        authUserId: user.userId
-    }
+  return {
+    authUserId: user.userId
+  };
 }
 
 /**
   * Register a user with an email, password, and names, then returns their authUserId value.
-  * 
+  *
   * @param {string} email - Email of user
   * @param {string} password - Password of user
-  * @param {string} nameFirst - First name of user 
+  * @param {string} nameFirst - First name of user
   * @param {string} nameLast - Last Name of user
-  * 
+  *
   * @returns {number} - Returns authUserId value when account is registered
 */
 
-export function adminAuthRegister( email, password, nameFirst, nameLast )
-{   
-    const checkEmail = validEmail(email);
-    if(checkEmail.error)
-        return{
-            error: checkEmail.error
-        }
+export function adminAuthRegister(email, password, nameFirst, nameLast) {
+  const checkEmail = validEmail(email);
+  if (checkEmail.error) {
+    return {
+      error: checkEmail.error
+    };
+  }
 
-    const checkPassword = validPassword(password);
-    if(checkPassword.error)
-        return{
-            error: checkPassword.error
-        }
+  const checkPassword = validPassword(password);
+  if (checkPassword.error) {
+    return {
+      error: checkPassword.error
+    };
+  }
 
-    const checkNameFirst = validName(nameFirst, true);
-    if(checkNameFirst.error)
-        return{
-            error: checkNameFirst.error
-        }
-    
-    const checkNameLast = validName(nameLast, false);
-    if(checkNameLast.error)
-        return{
-            error: checkNameLast.error
-        }     
+  const checkNameFirst = validName(nameFirst, true);
+  if (checkNameFirst.error) {
+    return {
+      error: checkNameFirst.error
+    };
+  }
 
-    let data = getData();
+  const checkNameLast = validName(nameLast, false);
+  if (checkNameLast.error) {
+    return {
+      error: checkNameLast.error
+    };
+  }
 
-    const newUser = {
-        userId: data.users.length + 1,
-        email: email,
-        name: nameFirst.concat(nameLast),
-        nameFirst: nameFirst,
-        nameLast: nameLast,
-        numFailedPasswordsSinceLastLogin: 0,
-        numSuccessfulLogins: 1,
-        oldPasswords: [],
-        ownedQuizzes: [],
-        password: password
-    }
+  const data = getData();
 
-    data.users.push(newUser);
+  const newUser = {
+    userId: data.users.length + 1,
+    email: email,
+    name: nameFirst.concat(nameLast),
+    nameFirst: nameFirst,
+    nameLast: nameLast,
+    numFailedPasswordsSinceLastLogin: 0,
+    numSuccessfulLogins: 1,
+    oldPasswords: [],
+    ownedQuizzes: [],
+    password: password
+  };
 
-    setData(data);
+  data.users.push(newUser);
 
-    return{ authUserId: newUser.userId }
+  setData(data);
+
+  return { authUserId: newUser.userId };
 }
 
 /**
   * Given an admin user's authUserId, return details about the user.
   * "name" is the first and last name concatenated with a single space between them.
-  * 
+  *
   * @param {number} authUserId - ID of user
-  * 
+  *
   * @returns {Object} - Returns an object containing details about the user when the account is logged in.
 */
 
-export function adminUserDetails(authUserId) 
-{
-    const checkAuthUserID = validAuthUserId(authUserId);
-    if(checkAuthUserID.error)
-        return{
-            error: checkAuthUserID.error
-        }     
-
+export function adminUserDetails(authUserId) {
+  const checkAuthUserID = validAuthUserId(authUserId);
+  if (checkAuthUserID.error) {
     return {
-        user: {
-            userId: checkAuthUserID.user.userId,
-            name: checkAuthUserID.user.nameFirst.concat(' ', checkAuthUserID.user.nameLast),
-            email: checkAuthUserID.user.email,
-            numSuccessfulLogins: checkAuthUserID.user.numSuccessfulLogins,
-            numFailedPasswordsSinceLastLogin: checkAuthUserID.user.numFailedPasswordsSinceLastLogin,
-            },
+      error: checkAuthUserID.error
     };
+  }
+
+  return {
+    user: {
+      userId: checkAuthUserID.user.userId,
+      name: checkAuthUserID.user.nameFirst.concat(' ', checkAuthUserID.user.nameLast),
+      email: checkAuthUserID.user.email,
+      numSuccessfulLogins: checkAuthUserID.user.numSuccessfulLogins,
+      numFailedPasswordsSinceLastLogin: checkAuthUserID.user.numFailedPasswordsSinceLastLogin,
+    },
+  };
 }
 
 /**
- * Given an admin user's authUserId and a set of properties, 
+ * Given an admin user's authUserId and a set of properties,
  * update the properties of this logged in admin user.
  * @param {number} authUserId - user ID
  * @param {string} email - email of user
  * @param {string} nameFirst - first name of user
  * @param {string} nameLast - last name of user
- * 
+ *
  * @returns {} - Returns empty object when admin user details is updated
  */
 
-export function adminUserDetailsUpdate( authUserId, email, nameFirst, nameLast ) 
-{
-    const checkAuthUserId = validAuthUserId(authUserId);
-    if(checkAuthUserId.error) 
-        return {
-            error: checkAuthUserId.error
-        }
-    
-    const checkEmail = validEmail(email);
-    if(checkEmail.error)
-        return{
-            error: checkEmail.error
-        }
+export function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+  const checkAuthUserId = validAuthUserId(authUserId);
+  if (checkAuthUserId.error) {
+    return {
+      error: checkAuthUserId.error
+    };
+  }
 
-    const checkNameFirst = validName(nameFirst, true);
-    if(checkNameFirst.error) 
-        return{
-            error: checkNameFirst.error
-        }
-    
-    const checkNameLast = validName(nameLast, false);
-    if(checkNameLast.error) 
-        return{
-            error: checkNameLast.error
-        } 
+  const checkEmail = validEmail(email);
+  if (checkEmail.error) {
+    return {
+      error: checkEmail.error
+    };
+  }
 
-    let data = getData();
-    
-    let user = data.users.find(user => user.userId === authUserId);
-    user.email = email;
-    user.nameFirst = nameFirst;
-    user.nameLast = nameLast;
+  const checkNameFirst = validName(nameFirst, true);
+  if (checkNameFirst.error) {
+    return {
+      error: checkNameFirst.error
+    };
+  }
 
-    setData(data);
+  const checkNameLast = validName(nameLast, false);
+  if (checkNameLast.error) {
+    return {
+      error: checkNameLast.error
+    };
+  }
 
-    return { }
+  const data = getData();
+
+  const user = data.users.find(user => user.userId === authUserId);
+  user.email = email;
+  user.nameFirst = nameFirst;
+  user.nameLast = nameLast;
+
+  setData(data);
+
+  return { };
 }
 
 /**
- * Given details relating to a password change, 
+ * Given details relating to a password change,
  * update the password of a logged in user.
- * 
+ *
  * @param {number} authUserId - ID of user
  * @param {string} oldPassword - Old password of user
  * @param {string} newPassword - New password of user
- * 
+ *
  * @returns {} - Returns empty object when password is updated.
  */
 
-export function adminUserPasswordUpdate( authUserId, oldPassword, newPassword ) 
-{
-    let data = getData()
+export function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
+  const data = getData();
 
-    const checkAuthUserId = validAuthUserId(authUserId);
-    if(checkAuthUserId.error) 
-        return {
-            error: checkAuthUserId.error
-        }
-    
-    let user = data.users.find(user => user.userId === authUserId)
+  const checkAuthUserId = validAuthUserId(authUserId);
+  if (checkAuthUserId.error) {
+    return {
+      error: checkAuthUserId.error
+    };
+  }
 
-    if (!(user.password === oldPassword)) 
-        return {
-            error: 'Old Password is not the correct old password'
-        }
-    
-    if (oldPassword === newPassword) 
-        return {
-            error: 'Old Password and New Password match exactly'
-        }
-    
-    const oldPasswordArray = user.oldPasswords
-    for (const oldPasswordUsed of oldPasswordArray) 
-        if (oldPasswordUsed === newPassword) 
-            return {
-                error: 'New Password has already been used before by this user'
-            }
-        
-    const checkPassword = validPassword(newPassword);
-    if(checkPassword.error)
-        return{
-            error: checkPassword.error
-        }
+  const user = data.users.find(user => user.userId === authUserId);
 
-    user.oldPasswords.push(oldPassword)
-    user.password = newPassword
+  if (!(user.password === oldPassword)) {
+    return {
+      error: 'Old Password is not the correct old password'
+    };
+  }
 
-    setData(data);
+  if (oldPassword === newPassword) {
+    return {
+      error: 'Old Password and New Password match exactly'
+    };
+  }
 
-    return { }
+  const oldPasswordArray = user.oldPasswords;
+  for (const oldPasswordUsed of oldPasswordArray) {
+    if (oldPasswordUsed === newPassword) {
+      return {
+        error: 'New Password has already been used before by this user'
+      };
+    }
+  }
+
+  const checkPassword = validPassword(newPassword);
+  if (checkPassword.error) {
+    return {
+      error: checkPassword.error
+    };
+  }
+
+  user.oldPasswords.push(oldPassword);
+  user.password = newPassword;
+
+  setData(data);
+
+  return { };
 }
--- src/other.js
+++ src/other.js
@@ -1,13 +1,13 @@
 import { getData, setData } from './dataStore.js';
 /**
   * Reset the state of the application back to the start.
-  * 
+  *
   * @param {} - no parameters
-  * 
+  *
   * @returns {} - returns an empty object when resetting the application to the start
 */
 export function clear() {
-  let currentState = getData();
+  const currentState = getData();
 
   currentState.users = [];
   currentState.quizzes = [];
