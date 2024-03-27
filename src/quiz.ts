34bb02f871db9bad49378424ad7b85ae3b55a7d0
Leon <z5480602@ad.unsw.edu.au>
2024-03-28 04:57:02 +1100

    Updated function header

--- src/quiz.ts
+++ src/quiz.ts
@@ -65,15 +65,14 @@ export function adminQuizCreate(token: string, name: string, description: string
 }
 
 /**
-  * Update the description of the relevant quiz.
+  * Update the name of the relevant quiz.
   *
-  * @param {number} authUserId - user id of admin
-  * @param {number} quizId - relevant quiz id
+  * @param {string} token - User ID of admin
+  * @param {number} quizid - relevant quizID
   * @param {string} description - new description for the relevant quiz
   *
-  * @returns {} - returns empty array when quiz description is updated
+  * @returns {object} - returns an empty object when a quiz is removed
 */
-
 export function adminQuizDescriptionUpdate(token: string, quizid: number, description: string): ErrorObject | object {
   const data = getData();
   const quizIndex = data.quizzes.findIndex(quizzes => quizzes.quizId === quizid);
