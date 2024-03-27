99500d135e6feefed2c7d9914f2cd86b52e6600c
Leon <z5480602@ad.unsw.edu.au>
2024-03-28 04:22:11 +1100

    QA FIX: Fixed quizids not being passed in correctly

--- src/server.ts
+++ src/server.ts
@@ -84,8 +84,9 @@ app.get('/v1/admin/user/details', (req: Request, res: Response) => {
 });
 
 app.put('/v1/admin/quiz/:quizid/name', (req: Request, res: Response) => {
-  const { token, quizid, name } = req.body;
-  const response = adminQuizNameUpdate(token, quizid, name);
+  const { token, name } = req.body;
+  const response = adminQuizNameUpdate(token, parseInt(req.params.quizid), name);
+
   if ('error' in response) {
     if (response.error === 'Quiz ID does not refer to a valid quiz.' || response.error === 'Quiz ID does not refer to a quiz that this user owns.') {
       return res.status(403).json(response);
