1aa709c5c8e3d0ff8eea1d27df2073aff12802df
Christian Politis <christianpolitis@gmail.com>
2024-03-27 16:38:24 +1100

    Updated data.md and datastore

--- src/other.ts
+++ src/other.ts
@@ -1,5 +1,5 @@
 import { getData, setData } from './dataStore';
-import type { DataStore } from './dataStore';
+import type { DataStore } from './interfaces';
 /**
   * Reset the state of the application back to the start.
   *
